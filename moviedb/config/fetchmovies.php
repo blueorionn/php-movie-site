197e1ca562d2e7f667c8a5dd3b9c3e006d302102
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-03 15:48:59 +0000

    added movies collection

---
+++ moviedb/config/fetchmovies.php
@@ -0,0 +1,35 @@
+<?php
+class Movies
+{
+    private $conn;
+
+    function __construct()
+    {
+        $host = getenv(name: 'DB_HOST');
+        $port = getenv(name: 'DB_PORT');
+        $dbname = getenv(name: 'DB_NAME');
+        $username = getenv(name: 'DB_USER');
+        $password = getenv(name: 'DB_PASS');
+
+        try {
+            $this->conn = new PDO(
+                dsn: "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+                username: $username,
+                password: $password,
+                options: [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Enable error handling
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Fetch results as an associative array
+                ]
+            );
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+        
+    }
+
+    public function getAllMovies(){
+        $stmt = $this->conn->prepare("SELECT * FROM movies");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
--- moviedb/index.php
+++ moviedb/index.php
@@ -1,3 +1,10 @@
+<?php
+require "config/fetchmovies.php";
+
+$mv = new Movies();
+$movies = $mv->getAllMovies();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -29,8 +36,40 @@
             </form>
         </section>
         <section class="w-full py-12 border-t-2 border-gray-200">
-            <div class="max-w-screen-2xl mx-auto">
+            <div class="max-w-screen-2xl mx-auto px-4">
                 <h1 class="text-xl font-semibold text-gray-600">Movies</h1>
+                <div class="my-8 w-full flex flex-wrap gap-16">
+                    <?php
+                    foreach ($movies as $movie) {
+                        $escaped_title = htmlspecialchars($movie['title'], ENT_QUOTES, 'UTF-8');
+
+                        echo "
+                        <a href='/update/{$movie['id']}' class='max-w-96 grid grid-cols-2 gap-4 group transition-all'>
+                            <div>
+                                <img src='/static/images/{$movie['poster']}' alt='$escaped_title' class='h-auto w-full aspect-auto'>
+                            </div>
+                            <div>
+                                <h1
+                                    class='text-2xl my-2 text-gray-800 font-semibold group-hover:underline'>
+                                    $escaped_title
+                                </h1>
+                                <!-- Released Date -->
+                                <h2 class='text-base text-gray-500 font-semibold inline-block'>{$movie['release_year']}</h2>
+                                <!-- Genre -->
+                                <span class='text-base mx-2 text-gray-600 font-normal'>{$movie['genre']}</span>
+                                <!-- Rating -->
+                                <span class='mx-2 sr-only'>Divider</span>
+                                <ion-icon name='star' style='color: goldenrod;'></ion-icon>
+                                <span class='text-base text-gray-600 font-normal'>{$movie['rating']}</span>
+                                <!-- Description -->
+                                <p class='text-base py-2.5 text-gray-700 font-normal group-hover:underline'>
+                                    {$movie['movie_description']}
+                                </p>
+                            </div>
+                        </a>";
+                    }
+                    ?>
+                </div>
             </div>
         </section>
     </main>
