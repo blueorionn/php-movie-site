e87048e99a64c3dc39582d805c218df9b97a1314
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-04 06:41:23 +0000

    Inheriting from parent class DatabaseConnection

--- moviedb/config/fetchmovies.php
+++ moviedb/config/fetchmovies.php
@@ -1,33 +1,10 @@
 <?php
-class Movies
+require 'connect.php';
+
+class Movies extends DatabaseConnection
 {
-    private $conn;
-
-    function __construct()
+    public function getAllMovies()
     {
-        $host = getenv(name: 'DB_HOST');
-        $port = getenv(name: 'DB_PORT');
-        $dbname = getenv(name: 'DB_NAME');
-        $username = getenv(name: 'DB_USER');
-        $password = getenv(name: 'DB_PASS');
-
-        try {
-            $this->conn = new PDO(
-                dsn: "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
-                username: $username,
-                password: $password,
-                options: [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Enable error handling
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Fetch results as an associative array
-                ]
-            );
-        } catch (PDOException $e) {
-            die("Database connection failed: " . $e->getMessage());
-        }
-        
-    }
-
-    public function getAllMovies(){
         $stmt = $this->conn->prepare("SELECT * FROM movies");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
